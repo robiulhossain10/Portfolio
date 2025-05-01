be4477f9952ec5d2f15fa9da302470b8c3c3963d
Robiul Hossain <munshearfat4@gmail.com>
2025-05-01 03:29:55 +0000

    footer add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       <footer className="py-6 md:px-8 md:py-0 border-t bg-secondary/50">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-20 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground">
-            Built by Alex Persona. © {new Date().getFullYear()}
+            Built by Robiul Hossain. © {new Date().getFullYear()}
           </p>
         </div>
       </footer>
